636a95e6acacb2ef6c2d1afc298e1ef55643b021
quintuskilbourn <47097067+quintuskilbourn@users.noreply.github.com>
2022-01-13 16:57:55 +0200

    Packed struct

--- OpenMultiSig.sol
+++ OpenMultiSig.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "contracts/MultiSig.sol";
+import "contracts/2_Owner.sol";
 
 contract OpenMultiSig is MultiSig {
   //
@@ -9,18 +9,18 @@ contract OpenMultiSig is MultiSig {
   //
 
   //call MultiSig constructor
-  constructor(uint[] memory _weights, address[] memory owners ,uint _thresh) payable MultiSig(_weights, owners, _thresh) {}
+  constructor(uint96[] memory _weights, address[] memory owners, uint128 _thresh) payable MultiSig(_weights, owners, _thresh) {}
 
   mapping(uint => mapping(address => bool)) public appVoted; //keep track of who voted for which txn
 
   struct Application {
     address applicant;
-    uint offer;     //how much they are offering to pay
-    uint votesRequested; 
+    uint96 offer;     //how much they are offering to pay, makes one slot in mem with applicant
+    uint96 votesRequested; 
+    uint128 proposedThresh; //may need new threshold
     bool accepted;
-    uint votes;  //how many tokens have voted for this
-    uint expiration;
-    uint proposedThresh; //may need new threshold
+    uint160 votes;  //how many tokens have voted for this
+    uint96 expiration;
   }
 
   Application[] public apps; 
@@ -46,17 +46,16 @@ contract OpenMultiSig is MultiSig {
   }
 
   event SubmitApplication(
-    address indexed applicant,
     uint indexed appIndex,
-    uint votesRequested,
-    uint offer,
-    uint proposedThresh
+    uint128 votesRequested,
+    uint96 offer,
+    uint128 proposedThresh
   );
 
   function submitApplication(
-    uint _offer,
-    uint _votesRequested,
-    uint _proposedThresh
+    uint96 _offer,
+    uint96 _votesRequested,
+    uint128 _proposedThresh
   ) public notOwner{
     require(_proposedThresh<= thresh + _votesRequested, "impossible threshold"); //else eth locked forever
     apps.push(
@@ -66,15 +65,15 @@ contract OpenMultiSig is MultiSig {
         votesRequested: _votesRequested,
         accepted: false,
         votes: 0,
-        expiration: block.timestamp + 1 weeks,
+        expiration: uint96(block.timestamp + 1 weeks),
         proposedThresh: _proposedThresh
       })
     );
-    emit SubmitApplication(msg.sender, apps.length-1, _votesRequested, _offer, _proposedThresh);
+    emit SubmitApplication( apps.length-1, _votesRequested, _offer, _proposedThresh);
   }
 
 
-  event VoteApplication(address indexed voter, uint indexed appIndex, uint votes); //indexed so can be searched
+  event VoteApplication(uint indexed appIndex, uint96 votes); //indexed so can be searched
 
   //vote to approve applicant, everyone votes at most once
   function voteApplication(uint _appIndex)
@@ -87,10 +86,10 @@ contract OpenMultiSig is MultiSig {
     Application storage application = apps[_appIndex];
     application.votes += weights[msg.sender];
     appVoted[_appIndex][msg.sender] = true;
-    emit VoteApplication(msg.sender, _appIndex, weights[msg.sender]);
+    emit VoteApplication(_appIndex, weights[msg.sender]);
   }
 
-  event AppRevokeVote(address indexed owner, uint indexed txIndex, uint votes); //indexed so searchable
+  event AppRevokeVote(uint indexed txIndex, uint96 votes); //indexed so searchable
 
   function appRevokeVote(uint _appIndex)
     public
@@ -102,10 +101,10 @@ contract OpenMultiSig is MultiSig {
     require(appVoted[_appIndex][msg.sender], "not voted yet");
     application.votes -= weights[msg.sender];
     appVoted[_appIndex][msg.sender] = false;
-    emit AppRevokeVote(msg.sender, _appIndex, weights[msg.sender]);
+    emit AppRevokeVote(_appIndex, weights[msg.sender]);
   }
 
-  event CompleteApplication(address indexed applicant, uint assigned_votes, uint paid);
+  event CompleteApplication(uint96 assigned_votes, uint paid);
 
   //if enough votes, add participant
   function completeApplication(uint _appIndex) payable external appExists(_appIndex) appNotAccepted(_appIndex) {
@@ -117,6 +116,6 @@ contract OpenMultiSig is MultiSig {
     weights[application.applicant] = application.votesRequested;
     totalWeight += application.votesRequested;
     thresh = application.proposedThresh;
+    emit CompleteApplication(application.votesRequested, msg.value);
   }
 }
-
